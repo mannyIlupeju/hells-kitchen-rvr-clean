e9c41a4053dd53792b9976f777e77868b3b56617
machnmb_ <mannybiz@outlook.com>
2025-07-15 08:57:11 -0400

    removed use of secrets from admin

--- src/infrastructure/db/firebase/firebaseAdmin.ts
+++ src/infrastructure/db/firebase/firebaseAdmin.ts
@@ -1,13 +1,19 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../../../serviceAccountkey.json';
+import dotenv from 'dotenv';
+dotenv.config();
 
 declare global {
-  var _firebaseAdminInitialized: boolean;
+  // Prevent re-initialization in serverless environments
+  var _firebaseAdminInitialized: boolean | undefined;
 }
 
 if (!global._firebaseAdminInitialized) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
   global._firebaseAdminInitialized = true;
